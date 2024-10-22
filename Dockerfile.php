6d77372fc9da025745a0beb3d85246ccb15cd945
Kevin Mensah <48464499+mamawpsg16@users.noreply.github.com>
2024-10-22 09:26:12 +0800

    add mysql directory for env
    add healtcheck in containers

--- Dockerfile.php
+++ Dockerfile.php
@@ -22,15 +22,15 @@ WORKDIR /var/www/html
 # Install Composer (PHP dependency manager)
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
+# Add health check
+HEALTHCHECK --interval=30s --timeout=10s --retries=3 \
+    CMD ["sh", "-c", "curl --fail http://localhost:9000 || exit 1"]
+
+# Expose port 9000 (used by PHP-FPM)
+EXPOSE 9000
 
 # Set the entrypoint
 ENTRYPOINT ["/bin/sh", "-c"]
 
 # Set the default command
 CMD ["if [ ! -d \"vendor\" ]; then composer install --no-interaction --no-plugins --no-scripts; fi && php-fpm"]
-
-# # Run Composer to install dependencies
-# RUN composer install
-
-# Expose port 9000 (used by PHP-FPM)
-EXPOSE 9000
